70a80850a5b3b0b14a8f95581ac5e0396336f990
aaguet <aaguet@guadalajara.gob.mx>
2022-12-20 10:52:05 -0600

    feat: :sparkles: Login implementado
    se activó el modulo facades en bootstrap/app.php

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\AuthService;
+
+class AuthController extends Controller
+{
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function authenticate()
+    {
+        $this->validate($this->request, [
+            'user'     => 'required',
+            'password'  => 'required'
+        ]);
+        $auth = AuthService::authenticate($this->request->user, $this->request->password);
+        return response()->json(['token' => $auth['data'],'message'=>$auth['message']], $auth['status']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,23 @@
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['middleware' => array('cors')], function () use ($router) {
+    $router->group(['prefix' => 'auth'], function ($router) {
+        $router->get('/login', 'AuthController@authenticate');
+    });
+});
+
 $router->group(['middleware' => array('cors')], function () use ($router) {
 
-    $router->get('/', function () use ($router) {
-        return $router->app->version();
-    });
     
-$router->group(['prefix' => 'vacantes'], function () use ($router) {
-    $router->get('/listado', 'VacanteController@getVacante');
-    $router->get('/eliminar/{id}', 'VacanteController@inhabilitar');
-    $router->get('/{id}', 'VacanteController@searchId');
-});
+    $router->group(['prefix' => 'vacantes'], function () use ($router) {
+        $router->get('/listado', 'VacanteController@getVacante');
+        $router->get('/eliminar/{id}', 'VacanteController@inhabilitar');
+        $router->get('/{id}', 'VacanteController@searchId');
+    });
 
 });
